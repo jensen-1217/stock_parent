8286d49d91803b3fc5e6351447b66f6ee7974d2a
jensen <594844561@qq.com>
2024-09-20 03:36:30 +0800

    day12第三次提交：配置shell脚本，自动化从远程仓库拉取代码maven字段打包，最终docker刷新镜像，启动容器

--- docker/bootStart.sh
+++ docker/bootStart.sh
@@ -15,7 +15,8 @@ cd ./docker
 echo 停止并删除相关容器
 output=`docker-compose down`
 echo 更新镜像,并启动容器
-output=`docker-compose up --build -d`
+output=`docker-compose build`
+output=`docker-compose up -d`
 echo 删除未被使用的镜像
 output=`docker image prune -f`
 echo 项目启动完成
